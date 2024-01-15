2909956528e8f3711614fb7ea1141d2266d0d17d
Marcos <marcsebasolinas@gmail.com>
2024-01-14 22:19:05 -0300

    agregue texto a html, se corrigieron algunos codigos del js como asi tambien se agrego mas codigo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 function ingreso() {
     let nombre = prompt("Ingrese su nombre");
         alert("Bienvenido: " + nombre)
+        console.log(nombre)
 }
 
 ingreso()
@@ -15,12 +16,16 @@ function subasta() {
     } else {
         if (valor < 600) {
             alert("Felicidades conseguiste unos: " + productos[0]);
+            console.log(productos[0])
         } else if (valor < 1000) {
             alert("Felicidades conseguiste un: " + productos[1]);
+            console.log(productos[1])
         } else if (valor < 1500) {
             alert("Felicidades conseguiste un: " + productos[2]);
+            console.log(productos[2])
         } else if (valor <= 2000) {
             alert("Felicidades conseguiste una " + productos[3]);
+            console.log(productos[3])
         } else {
             alert("No tenemos un producto para semejante dineral!");
         }
@@ -33,11 +38,13 @@ function envio(){
     let direccion = (prompt("Ingrese su direccion para enviar dicho producto y numero de telefono"))
     if (direccion != null && valor <= 2000){
         alert("Ni bien despachemos el producto lo estaremos llamando, gracias!")
+        console.log(direccion)
     }else{
         alert("Ingreso un monto que supera el valor de un producto, o no ingreso un dato de relevancia")
 }
 }
 
 if (valor <= 2000) {
-    envio(); 
+    envio();
+    console.log(valor) 
 }
