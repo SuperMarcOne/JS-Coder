68cc450b03fd9d06e4d20f95dae0853c85a41cc7
Marcos <marcsebasolinas@gmail.com>
2024-01-14 21:10:57 -0300

    se agregan mas funciones y se corrigen algunos errores

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-let edad = parseInt(prompt("Ingrese su edad"))
-
-function ingreso(){
-    if (edad >= 18){
-        alert("Buenisimo eres mayor! puedes ingresar")
-    }else{
-        alert("Lo sentimos, eres menor, no podras ingresar")
-    }
+function ingreso() {
+    let nombre = prompt("Ingrese su nombre");
+        alert("Bienvenido: " + nombre)
 }
 
 ingreso()
 
 const productos = [ "auriculares", "celular", "televisor 4k", "notebook"]
 
-let valor = parseInt(prompt("Ingrese un monto en usd para obtener un producto"))
+let valor = parseInt(prompt("Ingrese un monto en usd para obtener un producto, NO MAYOR A 2000 USD!"))
 
-function subasta(){
-    if (valor < 250){
-        confirm("Sos rata o te haces?")
-    }else if(valor >= 250){
-        alert("Felicidades conseguiste unos: " + productos[0])
-    }else if(valor >= 600){
-        alert("Felicidades conseguiste un: " + productos[1])
-    }else if(valor >= 1000){
-        alert("Felicidades conseguiste un: " + productos[2])
-    }else if(valor >= 1500){
-        alert("Felicidades conseguiste una " + productos[3])
-    }else if(valor >= 2000){
-        alert("No tenemos un producto para semejante dineral!")
+function subasta() {
+    if (valor < 250) {
+        confirm("Sos rata o te haces?");
+    } else {
+        if (valor < 600) {
+            alert("Felicidades conseguiste unos: " + productos[0]);
+        } else if (valor < 1000) {
+            alert("Felicidades conseguiste un: " + productos[1]);
+        } else if (valor < 1500) {
+            alert("Felicidades conseguiste un: " + productos[2]);
+        } else if (valor <= 2000) {
+            alert("Felicidades conseguiste una " + productos[3]);
+        } else {
+            alert("No tenemos un producto para semejante dineral!");
+        }
     }
 }
 
 subasta()
 
-let direccion = (prompt("Ingrese su direccion para enviar dicho producto y numero de telefono"))
-
 function envio(){
-    if (direccion != null){
+    let direccion = (prompt("Ingrese su direccion para enviar dicho producto y numero de telefono"))
+    if (direccion != null && valor <= 2000){
         alert("Ni bien despachemos el producto lo estaremos llamando, gracias!")
-    }else("No ingreso nada vuelva a intentarlo")
+    }else{
+        alert("Ingreso un monto que supera el valor de un producto, o no ingreso un dato de relevancia")
+}
 }
 
-envio()
+if (valor <= 2000) {
+    envio(); 
+}
